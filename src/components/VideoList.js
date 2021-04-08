1cfbdbac86d4b370f99432825a95109e8f02587b
thraddash <theartist@gmail.com>
2021-04-08 13:56:26 -0700

    take video property out of props object in VideoList, remove repetitive props wording

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
-const VideoList = (props) => {
-    // pass this.state.videos from App.js down to Videolist props
-    // props.videos 
-    return <div>VideoList {props.videos.length}</div>
+// pass this.state.videos from App.js down to Videolist props
+// props.videos 
+
+// destructure using ES2015 syntax, take the videos property out of the props object
+// by converting const VideoList = (props) to  ({ videos })
+const VideoList = ({ videos }) => {
+    
+    return <div>VideoList {videos.length}</div>
 };
 
 export default VideoList;
