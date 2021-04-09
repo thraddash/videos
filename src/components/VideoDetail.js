b1a75cb415720102ae3e8f950a25e6a97742c72c
thraddash <theartist@gmail.com>
2021-04-08 19:30:01 -0700

    add video player

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -4,8 +4,14 @@ const VideoDetail = ({ video }) => {
     if (!video){
         return <div>Loading...</div>
     }
+
+    const videoSrc= `https://www.youtube.com/embed/${video.id.videoId}`
+
     return (
         <div className="ui segment">
+            <div className="ui embed">
+                <iframe src={videoSrc} />
+            </div>
             <h4 className="ui header">{video.snippet.title}</h4>
             <p>{video.snippet.description}</p>
         </div>
