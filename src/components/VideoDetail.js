7b10919fa1417a0ddc8a9c76362773bceb7d1f55
thraddash <theartist@gmail.com>
2021-04-08 19:33:29 -0700

    fix iframe title

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -10,7 +10,7 @@ const VideoDetail = ({ video }) => {
     return (
         <div className="ui segment">
             <div className="ui embed">
-                <iframe src={videoSrc} />
+                <iframe title="video player" src={videoSrc} />
             </div>
             <h4 className="ui header">{video.snippet.title}</h4>
             <p>{video.snippet.description}</p>
